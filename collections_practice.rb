21b721ad82181220d932c6c8b6c46b0d0374d3ac
Pedro Huertas <pmh242@gmail.com>
2019-11-26 05:17:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,11 +14,8 @@ def swap_elements(array)
   array[0], array[1], array[2] = array[0], array[2], array[1]
 end
 
-
-
 def kesha_maker(array)
-  new_array = []
   array.each do |name|
-    new_array << name.length
+    name[2] = "$"
   end
 end
