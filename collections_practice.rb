14b6a4280ad3133c4bceaea6bde990a9bc0640f2
Pedro Huertas <pmh242@gmail.com>
2019-11-26 05:17:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(array)
+  array.sort!
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |name|
+    new_array << name.length
+  end
+end
