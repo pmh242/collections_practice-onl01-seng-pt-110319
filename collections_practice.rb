a6a164f6a2060a0ae7596e7b4558c329a153a97f
Pedro Huertas <pmh242@gmail.com>
2019-11-26 05:19:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,12 +14,12 @@ def swap_elements(array)
   array[0], array[1], array[2] = array[0], array[2], array[1]
 end
 
+def reverse_array(array)
+  array.reverse
+end
+
 def kesha_maker(array)
   array.each do |name|
     name[2] = "$"
   end
-end
-
-def find_a
-  
 end
